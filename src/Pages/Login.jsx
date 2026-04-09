9e1a5e8bcd82250e54d83df349c0cd600711545d
chiddesh <chiddesh@gmail.com>
2026-04-09 19:15:09 +0530

    MADE CHANGES

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,6 +4,11 @@ function Login() {
     return (
         <div>
             <h1>Ranipet District</h1>
+            <div>
+                <h1>HOME</h1>
+                <h1>PLEDGE</h1>
+                <h1>CONTACT</h1>
+            </div>
         </div>
     )
 }
