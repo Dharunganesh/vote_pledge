d8046d6b2c80ab090fde3c57eba6b78aa48533ac
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-09 20:48:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,1 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Login from "./Pages/Login";
-import OTPVerification from "./Pages/OTPVerification";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/otp" element={<OTPVerification />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
