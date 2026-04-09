9db5df1968a08a640117c2c97391a4864d288327
chiddesh <chiddesh@gmail.com>
2026-04-09 20:45:21 +0530

    made the login page with number useState

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import './App.css'
 function App() {
     const [phoneNumber, setPhoneNumber] = useState('')
 
-    const submitPhoneNumber = () => {
+    const submitPhoneNumber = (e) => {
+        e.preventDefault()
+        const regex = /[^0-9]/g
+        if (phoneNumber.length < 10 || regex.test(phoneNumber)) alert("தயவுசெய்து சரியான மொபைல் எண்ணை உள்ளிடவும்")
         console.log(phoneNumber)
     }
 
