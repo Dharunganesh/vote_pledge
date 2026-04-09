2777701aff246201cfa953fc4d755d7c053eb329
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-09 20:49:17 +0530

    Implement phone number input and validation
    Added phone number state management and validation in App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
 
+import React, { useState } from 'react'
+import Login from './Pages/Login'
+import './App.css'
+
+function App() {
+    const [phoneNumber, setPhoneNumber] = useState('')
+
+    const submitPhoneNumber = (e) => {
+        e.preventDefault()
+        const regex = /[^0-9]/g
+        if (phoneNumber.length < 10 || regex.test(phoneNumber)) alert("தயவுசெய்து சரியான மொபைல் எண்ணை உள்ளிடவும்")
+        console.log(phoneNumber)
+    }
+
+    return (
+        <div>
+            <Login phoneNumber={phoneNumber} setPhoneNumber={setPhoneNumber} submitPhoneNumber={submitPhoneNumber} />
+        </div>
+    )
+}
+
+export default App
