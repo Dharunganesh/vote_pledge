54e82fecb8589f772d4cfc5ad65fe7d4ce054e73
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-09 20:34:08 +0530

    updated app.jsx to include routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+<<<<<<< Updated upstream
 import React from 'react'
 import Login from './Pages/Login'
 import './App.css'
+=======
+import Login from "./Pages/Login";
+import OTPVerification from "./Pages/OTPVerification";
+>>>>>>> Stashed changes
 
 function App() {
-    return (
-        <div>
-            <Login />
-        </div>
-    )
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/otp" element={<OTPVerification />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
