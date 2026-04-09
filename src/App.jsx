f9cb4a62da4d2ecf5ca35ee8820ab9c4994bfbef
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-09 20:38:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-<<<<<<< Updated upstream
-import React from 'react'
-import Login from './Pages/Login'
-import './App.css'
-=======
 import Login from "./Pages/Login";
 import OTPVerification from "./Pages/OTPVerification";
->>>>>>> Stashed changes
 
 function App() {
   return (
