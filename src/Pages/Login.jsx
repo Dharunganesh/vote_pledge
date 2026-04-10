2d4707bcf634df7f88ea74ab7fe9ac621233da95
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-10 19:36:01 +0530

    removed phone number verification

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -3,23 +3,30 @@ import Navbar from '../Components/Navbar'
 import mascot from '../assets/mascot.png'
 import roundLogo from '../assets/round-logo.png'
 import ecilogo from '../assets/eci-logo.png'
+import { useNavigate } from "react-router-dom"
 
-function Login({ setPhoneNumber, phoneNumber, submitPhoneNumber }) {
+function Login() {
+    const navigate = useNavigate()
+
+    const handleEnter = () => {
+        navigate("/home") // change this route if needed
+    }
+
     return (
         <div className='min-h-screen bg-neutral-200 relative'>
             <Navbar />
+
             <div className='flex justify-between'>
                 <img
                     src={ecilogo}
-                    alt="mascot"
+                    alt="logo"
                     className='absolute left-4 top-27 w-30 md:w-40 md:right-10 md:top-28 drop-shadow-lg z-1'
                 />
                 <img
                     src={roundLogo}
-                    alt="mascot"
+                    alt="logo"
                     className='absolute right-3 top-24 w-34 md:w-40 md:right-10 md:top-28 drop-shadow-lg z-1'
                 />
-
             </div>
 
             <div className='flex flex-col items-center px-5 py-6 z-10 relative'>
@@ -29,28 +36,16 @@ function Login({ setPhoneNumber, phoneNumber, submitPhoneNumber }) {
                 </h1>
 
                 <p className='text-center text-gray-900 mt-5 text-xl md:text-2xl'>
-                    வாக்களிப்பு உறுதிமொழி எடுக்க உங்கள் மொபைல் எண்ணை உள்ளிடவும்
+                    வாக்களிப்பு உறுதிமொழி எடுக்க கீழே உள்ள பொத்தானை அழுத்தவும்
                 </p>
 
                 <div className='bg-white w-full max-w-md mt-5 p-4 rounded-2xl shadow md:max-w-xl md:mt-20'>
 
-                    <label className='text-xs font-semibold text-gray-700'>
-                        மொபைல் எண் / Mobile Number
-                    </label>
-
-                    <input
-                        type="text"
-                        placeholder="+91 98765 43210"
-                        className='w-full mt-2 mb-2 p-3 rounded-lg bg-gray-100 outline-none md:mb-5'
-                        value={phoneNumber}
-                        onChange={(e) => setPhoneNumber(e.target.value)}
-                    />
-
                     <button
                         className='w-full mt-4 bg-orange-600 text-white py-4 rounded-xl font-semibold md:mb-5'
-                        onClick={() => submitPhoneNumber()}
+                        onClick={handleEnter}
                     >
-                        OTP அனுப்பு / Send OTP
+                        உள்ள்நுழைக / Enter
                     </button>
 
                     <div className='flex items-center justify-between'>
@@ -63,12 +58,9 @@ function Login({ setPhoneNumber, phoneNumber, submitPhoneNumber }) {
                             OFFICIAL PORTAL<br />
                             <span className='text-black font-bold'>Govt of Tamil Nadu</span>
                         </p>
-
                     </div>
 
-
                 </div>
-
             </div>
         </div>
     )
