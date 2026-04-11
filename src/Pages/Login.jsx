9c757890e5903bf94bb39747a813b2290cef4b65
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-12 00:01:29 +0530

    Update Login.jsx

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -8,7 +8,7 @@ function Login() {
     const navigate = useNavigate()
 
     const handleEnter = () => {
-        navigate("/home") // change if needed
+        navigate("/userDetails")
     }
 
     return (
