e260e4b92c9705e12eb4eef4365521c3fdbbb685
Dharunganesh <147619802+Dharunganesh@users.noreply.github.com>
2026-04-11 10:49:59 +0530

    Update login navigation and UI elements

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import mascot from '../assets/mascot.png'
 import roundLogo from '../assets/round-logo.png'
 import ecilogo from '../assets/eci-logo.png'
 import { useNavigate } from "react-router-dom"
@@ -8,19 +9,17 @@ function Login() {
     const navigate = useNavigate()
 
     const handleEnter = () => {
-        navigate("/home") // change if needed
+        navigate("/userDetails")
     }
 
     return (
-        <div className='min-h-screen bg-neutral-200 flex flex-col relative pb-38'>
-            
+        <div className='min-h-screen bg-neutral-200 relative'>
             <Navbar />
 
-            {/* Logos */}
             <div className='flex justify-between'>
                 <img
                     src={ecilogo}
-                    alt="logo"
+                    alt="mascot"
                     className='absolute left-4 top-27 w-30 md:w-40 md:right-10 md:top-28 drop-shadow-lg z-1'
                 />
                 <img
@@ -30,47 +29,40 @@ function Login() {
                 />
             </div>
 
-            {/* Main Content */}
-            <div className='flex-grow flex flex-col items-center px-5 py-6 z-10'>
+            <div className='flex flex-col items-center px-5 py-6 z-10 relative'>
 
                 <h1 className='text-2xl font-bold text-center px-5 py-5 rounded-2xl mt-30 text-orange-600 md:text-4xl'>
                     உங்கள் வாக்கு நாட்டின் வலிமை
                 </h1>
 
                 <p className='text-center text-gray-900 mt-5 text-xl md:text-2xl'>
-                    வாக்களிப்பு உறுதிமொழி எடுக்க கீழே உள்ள பொத்தானை அழுத்தவும்
+                    தொடர கீழே உள்ள பட்டனை அழுத்தவும்
                 </p>
 
-                {/* Card */}
                 <div className='bg-white w-full max-w-md mt-5 p-4 rounded-2xl shadow md:max-w-xl md:mt-20'>
 
                     <button
                         className='w-full mt-4 bg-orange-600 text-white py-4 rounded-xl font-semibold md:mb-5'
                         onClick={handleEnter}
                     >
-                        உள்ள்நுழைக / Enter
+                        Enter
                     </button>
 
-                </div>
-            </div>
+                    <div className='flex items-center justify-between'>
+                        <p className='mt-5 text-center'>
+                            VOTER HELPLINE <br />
+                            <span className='text-red-500 font-extrabold'>1950</span>
+                        </p>
 
-            {/* Bottom Section (Above Navbar) */}
-            <div className='w-full px-5 mb-4'>
-                <div className='flex items-center justify-between'>
-
-                    <p className='text-center text-sm'>
-                        VOTER HELPLINE <br />
-                        <span className='text-red-500 font-extrabold'>1950</span>
-                    </p>
-
-                    <p className='text-center text-sm'>
-                        OFFICIAL PORTAL<br />
-                        <span className='text-black font-bold'>Govt of Tamil Nadu</span>
-                    </p>
+                        <p className='mt-5 text-center'>
+                            OFFICIAL PORTAL<br />
+                            <span className='text-black font-bold'>Govt of Tamil Nadu</span>
+                        </p>
+                    </div>
 
                 </div>
-            </div>
 
+            </div>
         </div>
     )
 }
